bb46b9e4175bbcb7ed8e31e4ee79d1e642b0d60d
masarray <ari.sulistiono@gmail.com>
2026-06-16 20:11:07 +0700

    Load mobile contact shadow guard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './game/runtimeFixes.js';
 import './game/smoothTrafficFixes.js';
 import './game/stageBalanceFixes.js';
 import './game/desktopShadowFixes.js';
+import './game/mobileContactShadowFixes.js';
 import App from './App.jsx';
 import './styles.css';
 
